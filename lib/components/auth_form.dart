19147e8a8bdf4ca7d706bd7c6103f1b61f3a882f
Thiago Nishimura <thiagonishimura@yahoo.com.br>
2023-05-11 15:40:33 -0300

    created the validation of the authentication form

--- lib/components/auth_form.dart
+++ lib/components/auth_form.dart
@@ -2,20 +2,28 @@ import 'package:chat/models/auth_form_data.dart';
 import 'package:flutter/material.dart';
 
 class AuthForm extends StatefulWidget {
-  const AuthForm({super.key});
+  final void Function(AuthFormData) onSubmit;
+
+  const AuthForm({
+    super.key,
+    required this.onSubmit,
+  });
 
   @override
   State<AuthForm> createState() => _AuthFormState();
 }
 
-final _formKey = GlobalKey<FormState>();
-final _formData = AuthFormData();
-
-void _submit() {
-  _formKey.currentState?.validate();
-}
-
 class _AuthFormState extends State<AuthForm> {
+  final _formKey = GlobalKey<FormState>();
+  final _formData = AuthFormData();
+
+  void _submit() {
+    final isValid = _formKey.currentState?.validate() ?? false;
+    if (!isValid) return;
+
+    widget.onSubmit(_formData);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -23,6 +31,7 @@ class _AuthFormState extends State<AuthForm> {
       child: Padding(
         padding: const EdgeInsets.all(16),
         child: Form(
+          key: _formKey,
           child: Column(
             children: [
               if (_formData.isSignup)
@@ -31,6 +40,13 @@ class _AuthFormState extends State<AuthForm> {
                   initialValue: _formData.name,
                   onChanged: (name) => _formData.name = name,
                   decoration: const InputDecoration(labelText: 'Name'),
+                  validator: (localName) {
+                    final name = localName ?? '';
+                    if (name.trim().length < 5) {
+                      return 'The name must be at least 5 characters!';
+                    }
+                    return null;
+                  },
                 ),
               TextFormField(
                 key: const ValueKey('email'),
@@ -38,6 +54,13 @@ class _AuthFormState extends State<AuthForm> {
                 onChanged: (email) => _formData.email = email,
                 keyboardType: TextInputType.emailAddress,
                 decoration: const InputDecoration(labelText: 'E-mail'),
+                validator: (localEmail) {
+                  final email = localEmail ?? '';
+                  if (!email.contains('@')) {
+                    return 'The e-mail is not valid!';
+                  }
+                  return null;
+                },
               ),
               TextFormField(
                 key: const ValueKey('password'),
@@ -45,6 +68,13 @@ class _AuthFormState extends State<AuthForm> {
                 onChanged: (password) => _formData.password = password,
                 obscureText: true,
                 decoration: const InputDecoration(labelText: 'Senha'),
+                validator: (localPassword) {
+                  final password = localPassword ?? '';
+                  if (password.length < 6) {
+                    return 'The password must be at least 6 characters!';
+                  }
+                  return null;
+                },
               ),
               const SizedBox(height: 10),
               ElevatedButton(
