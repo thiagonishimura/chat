e3be2a5dab8e8b081f60392a0054f61e34c8d89f
Thiago Nishimura <thiagonishimura@yahoo.com.br>
2023-05-12 14:57:59 -0300

    created simulated authentication

--- lib/components/auth_form.dart
+++ lib/components/auth_form.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 import 'package:chat/components/user_image_picker.dart';
-import 'package:chat/models/auth_form_data.dart';
+import 'package:chat/core/models/auth_form_data.dart';
 import 'package:flutter/material.dart';
 
 class AuthForm extends StatefulWidget {
@@ -47,6 +47,7 @@ class _AuthFormState extends State<AuthForm> {
   @override
   Widget build(BuildContext context) {
     return Card(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
       margin: const EdgeInsets.all(20),
       child: Padding(
         padding: const EdgeInsets.all(16),
