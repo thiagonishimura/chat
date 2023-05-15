cd9c9185b319df2fbd8749e40125efbea4fc2441
Thiago Nishimura <thiagonishimura@yahoo.com.br>
2023-05-15 11:52:56 -0300

    Created the service of notification message

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:chat/core/services/notification/push_notification_service.dart';
 import 'package:chat/pages/auth_or_app_page.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MainApp());
@@ -10,13 +12,20 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Chat',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => PushNotificationService(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Chat',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const AuthOrAppPage(),
       ),
-      home: const AuthOrAppPage(),
     );
   }
 }
