b4c0ec199fd703398a4db8f666f264144b9252dd
Thiago Nishimura <thiagonishimura@yahoo.com.br>
2023-05-15 11:20:04 -0300

    Performed action of send a new message

--- lib/core/services/chat/chat_mock_service.dart
+++ lib/core/services/chat/chat_mock_service.dart
@@ -5,32 +5,7 @@ import 'package:chat/core/models/chat_user.dart';
 import 'package:chat/core/services/chat/chat_service.dart';
 
 class ChatMockService implements ChatService {
-  static final List<ChatMessage> _msgs = [
-    ChatMessage(
-      id: '1',
-      text: 'Good morning',
-      createdAt: DateTime.now(),
-      userId: '123',
-      userName: 'Bia',
-      userImageUrl: 'assets/images/avatar.png',
-    ),
-    ChatMessage(
-      id: '2',
-      text: 'Good mornig. Will we have a meeting today?',
-      createdAt: DateTime.now(),
-      userId: '456',
-      userName: 'Ana',
-      userImageUrl: 'assets/images/avatar.png',
-    ),
-    ChatMessage(
-      id: '3',
-      text: 'Yes. Can it be now!',
-      createdAt: DateTime.now(),
-      userId: '123',
-      userName: 'Bia',
-      userImageUrl: 'assets/images/avatar.png',
-    ),
-  ];
+  static final List<ChatMessage> _msgs = [];
 
   static MultiStreamController<List<ChatMessage>>? _controller;
   static final _msgsStream = Stream<List<ChatMessage>>.multi((controller) {
@@ -54,7 +29,7 @@ class ChatMockService implements ChatService {
       userImageUrl: user.imageUrl,
     );
     _msgs.add(newMessage);
-    _controller?.add(_msgs);
+    _controller?.add(_msgs.reversed.toList());
     return newMessage;
   }
 }
