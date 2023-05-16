f3652ccb04db502940f03b0b550f6ef218dbc02f
Thiago Nishimura <thiagonishimura@yahoo.com.br>
2023-05-16 11:37:54 -0300

    Chat message save in firebase

--- lib/core/services/chat/chat_service.dart
+++ lib/core/services/chat/chat_service.dart
@@ -1,12 +1,14 @@
 import 'package:chat/core/models/chat_message.dart';
 import 'package:chat/core/models/chat_user.dart';
-import 'package:chat/core/services/chat/chat_mock_service.dart';
+import 'package:chat/core/services/chat/chat_firebase_service.dart';
+//import 'package:chat/core/services/chat/chat_mock_service.dart';
 
 abstract class ChatService {
   Stream<List<ChatMessage>> messagesStream();
-  Future<ChatMessage> save(String texto, ChatUser user);
+  Future<ChatMessage?> save(String texto, ChatUser user);
 
   factory ChatService() {
-    return ChatMockService();
+    return ChatFirebaseService();
+    //return ChatMockService();
   }
 }
