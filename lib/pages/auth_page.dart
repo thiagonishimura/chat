5dd089b579cc0d0ef61d4350e43442106efaca3c
Thiago Nishimura <thiagonishimura@yahoo.com.br>
2023-05-16 09:49:34 -0300

    Created the Firebase service to send the profile image

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -15,6 +15,7 @@ class _AuthPageState extends State<AuthPage> {
 
   Future<void> _handleSubmit(AuthFormData formData) async {
     try {
+      if (!mounted) return;
       setState(() => _isLoading = true);
 
       if (formData.isLogin) {
@@ -35,6 +36,7 @@ class _AuthPageState extends State<AuthPage> {
     } catch (error) {
       // Tratar erro!
     } finally {
+      if (!mounted) return;
       setState(() => _isLoading = false);
     }
   }
